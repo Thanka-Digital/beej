3350af990af6030a6e91b992f54bcc58a49789a7
Alson Garbuja <magar33alson@gmail.com>
2024-10-27 22:42:47 +0545

    page: basic page setup

--- templates/react/tailwind-context-fetch/src/pages/HomePage.tsx
+++ templates/react/tailwind-context-fetch/src/pages/HomePage.tsx
@@ -1,11 +1,52 @@
-// Layouts
+import { toast } from "sonner";
+
+// LAYOUTS
 import MaxWidthWrapper from "@/layout/wrapper/MaxWidthWrapper";
 
+// COMPONENTS
+import Button from "@/components/form/button/Button";
+import Tooltip from "@/components/global/tooltip/Tooltip";
+
+// CUSTOM HOOKS
+import { useModal } from "@/hooks/portal/useModal";
+
 export default function HomePage() {
+  const { openPortal, PortalModal } = useModal({
+    message: "Open the toast?",
+    positive: {
+      text: "Yes",
+      colorScheme: "success",
+      className: "font-semibold text-white",
+      onClick: () => toast.success("Toast opened"),
+    },
+    negative: {
+      text: "No",
+      colorScheme: "danger",
+      className: "font-semibold text-white",
+      onClick: () => {},
+    },
+  });
+
   return (
     <main>
-      <MaxWidthWrapper className="flex items-center justify-center w-screen h-screen">
-        <h1>Start by editing files in pages folder</h1>
+      <MaxWidthWrapper className="flex flex-col items-center justify-center w-screen h-screen gap-2">
+        <h1 className="text-3xl font-bold">
+          Start by editing files in pages folder
+        </h1>
+
+        <Tooltip message="Click to open modal">
+          <Button
+            className="font-semibold rounded-md"
+            padding="base"
+            colorscheme="danger"
+            variant="ghost"
+            onClick={openPortal}
+          >
+            Hover me
+          </Button>
+        </Tooltip>
+
+        <PortalModal />
       </MaxWidthWrapper>
     </main>
   );
